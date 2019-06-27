fb6d8222cdbba1bd165a8f3de33872127361e095
ririgi <lrpbaltazar@gmail.com>
2019-06-27 10:58:13 +0800

    finish mca script

--- mca.R
+++ mca.R
@@ -1,15 +1,24 @@
 library("FactoMineR")
 library("factoextra")
 
-df <- read.csv("data.csv")
+df <- read.csv("november_2018.csv")
 head(df)
 row.names(df) <- df$gigyaid
 df$gigyaid <- NULL
-colnames(df)
 
 res.mca <- MCA(df, graph = FALSE)
 eigenvalues <- get_eigenvalue(res.mca)
 plot(fviz_screeplot(res.mca, addlabels = TRUE, ylim = c(0, 45)))
 plot(fviz_mca_var(res.mca, choice = "mca.cor", 
-             repel = TRUE, # Avoid text overlapping (slow)
              ggtheme = theme_minimal()))
+plot(fviz_mca_var(res.mca, ggtheme = theme_minimal()))
+
+library("corrplot")
+corrplot(var$cos2, is.corr=FALSE)
+
+plot(fviz_mca_var(res.mca,
+             ggtheme = theme_minimal()))
+
+plot(fviz_cos2(res.mca, choice = "var", axes = 1))
+plot(fviz_contrib(res.mca, choice = "var", axes = 1, top = 30))
+plot(fviz_contrib(res.mca, choice = "var", axes = 2, top = 30))
